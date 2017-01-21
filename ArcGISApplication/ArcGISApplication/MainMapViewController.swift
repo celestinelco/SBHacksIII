1aac2d7070beb0dcc6d30cb17da17685ce0f9ff0
Celestine Co <celestine@169-231-156-116.wireless.ucsb.edu>
2017-01-21 04:10:08 -0800

    Initialized map view

---
+++ ArcGISApplication/ArcGISApplication/MainMapViewController.swift
@@ -0,0 +1,25 @@
+//
+//  MainMapViewController.swift
+//  ArcGISApplication
+//
+//  Created by Celestine Co on 1/21/17.
+//  Copyright © 2017 Celestine. All rights reserved.
+//
+
+import Foundation
+import UIKit
+import ArcGIS
+
+class MainMapViewController: UIViewController {
+    
+    
+    @IBOutlet weak var myMap: AGSMapView!
+    
+    override func viewDidLoad() {
+        print("\n\nMainMapViewController.swift\n\n")
+        
+        myMap.map = AGSMap(basemapType: .imageryWithLabels, latitude: 35.3050, longitude: -120.6625, levelOfDetail: 16)
+    }
+    
+    
+}
