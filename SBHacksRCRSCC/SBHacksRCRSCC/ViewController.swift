bbb635a4cc3cd66c62031a4a50254eac217afab9
Celestine Co <celestine@Celestines-MacBook-Air.local>
2017-01-21 01:04:46 -0800

    Set up ArcGIS Bundle

---
+++ SBHacksRCRSCC/SBHacksRCRSCC/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  SBHacksRCRSCC
+//
+//  Created by Celestine Co on 1/21/17.
+//  Copyright © 2017 Celestine. All rights reserved.
+//
+
+import UIKit
+import ArcGIS
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
